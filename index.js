9ec2d84d92e104da74c28f4cb8750965a2ce0d65
Nikhil Patil <nikhilpatil2005@gmail.com>
2018-11-27 10:50:29 +0530

    First commit

---
+++ index.js
@@ -0,0 +1,31 @@
+const express = require("express")
+const app = express()
+const port = 3000
+
+app.use(express.json())
+
+app.use((request, response, next) => {
+    console.log("Request received", request.body)
+    next()
+})
+
+app.post('/', (request, response) => {
+    if (!request.body.hasOwnProperty("foo"))
+        throw new Error("`foo` expected in body")
+
+    if (request.body.foo == "bar") {
+        console.log("`foo-bar` matched. We likes it!")
+        response.status(200).send(request.body)
+    }
+    else {
+        console.log("Expected value for `foo` is `bar`. Received : ", request.body.foo)
+        throw new Error("`foo` was sent with wrong value. We dislikes it!")
+    }
+})
+
+app.use((err, request, response, next) => {
+    console.log(err)
+    response.status(500).send({ error: err.toString()})
+})
+
+app.listen(port)
